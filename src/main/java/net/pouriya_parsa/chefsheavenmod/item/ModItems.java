a39158371663ccc48eb821a4a227432f2f763e22
Parsakhk <parsakhk88@gmail.com>
2022-08-28 16:48:26 +0430

    Cooked Tomato and Its recipe

--- src/main/java/net/pouriya_parsa/chefsheavenmod/item/ModItems.java
+++ src/main/java/net/pouriya_parsa/chefsheavenmod/item/ModItems.java
@@ -1,5 +1,7 @@
 package net.pouriya_parsa.chefsheavenmod.item;
 
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -18,6 +20,10 @@ public class ModItems {
     public static final RegistryObject<Item> TOMATO = ITEMS.register("tomato",
             () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CHEFS_HEAVEN_MOD_TAB)
                     .food(new FoodProperties.Builder().nutrition(3).build())));
+
+    public static final RegistryObject<Item> COOKED_TOMATO = ITEMS.register("cooked_tomato",
+            () -> new Item(new Item.Properties().tab(ModCreativeModeTab.CHEFS_HEAVEN_MOD_TAB)
+                    .food(new FoodProperties.Builder().nutrition(3).saturationMod(0.3F).effect(new MobEffectInstance(MobEffects.SATURATION, 60, 1), 2.0F).build())));
 
 
     public static void register(IEventBus eventBus) {
