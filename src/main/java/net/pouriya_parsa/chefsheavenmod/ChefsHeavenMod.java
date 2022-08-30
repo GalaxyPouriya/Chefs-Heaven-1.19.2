27ead0b6507f1427652722d958d6e092049ee1fd
Galaxy_Pouriya <galaxypouriya@gmail.com>
2022-08-30 12:24:54 +0430

    Block adding ready

--- src/main/java/net/pouriya_parsa/chefsheavenmod/ChefsHeavenMod.java
+++ src/main/java/net/pouriya_parsa/chefsheavenmod/ChefsHeavenMod.java
@@ -8,6 +8,7 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.pouriya_parsa.chefsheavenmod.block.ModBlocks;
 import net.pouriya_parsa.chefsheavenmod.item.ModItems;
 import org.slf4j.Logger;
 
@@ -21,6 +22,7 @@ public class ChefsHeavenMod {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
